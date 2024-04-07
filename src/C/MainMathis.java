9ca3e38d95efdeb2734f17859d0969f30437da7f
Poulereaper <104522240+Poulereaper@users.noreply.github.com>
2024-04-07 17:14:28 +0200

    Add TestEnfFrameBase import in MainMathis.java

--- src/C/MainMathis.java
+++ src/C/MainMathis.java
@@ -1,6 +1,7 @@
 package C;
 
 import V.testAffichageMathis;
+import V.TestEnfFrameBase;
 import java.awt.Color;
 
 public class MainMathis {
@@ -8,5 +9,6 @@ public class MainMathis {
     {
         testAffichageMathis test = new testAffichageMathis();
         test.affichage((new Color(250, 250, 250)), (new Color(0, 19, 77)), (new Color(0, 0, 0)));
+        //TestEnfFrameBase test2 = new TestEnfFrameBase();
     }
 }
---
+++ src/V/TestEnfFrameBase.java
@@ -0,0 +1,16 @@
+package V;
+
+//Importation des fichiers
+
+//Importation des librairies
+import javax.swing.JFrame;
+import java.awt.Color;
+
+public class TestEnfFrameBase extends FrameBase{
+
+
+    public TestEnfFrameBase(){
+        super("teste", new Color(250, 250, 250), new Color(0, 19, 77), new Color(0, 0, 0));
+    }
+    
+}
