8bec37530d1e5201439ddedb079717399df94ab9
Ashtaggg <alexis1.raynal@gmail.com>
2024-04-07 19:57:53 +0200

    page de connexion

--- src/C/MainAlexis.java
+++ src/C/MainAlexis.java
@@ -1,12 +1,18 @@
 package C;
 
 import V.testAffichageAlexis;
+import M.Utilisateur.Connexion;
 
 public class MainAlexis {
     public static void main(String[] ags)
     {
         testAffichageAlexis test = new testAffichageAlexis();
         test.affichage();
+    }
+
+    public void boutonConnexion(String mail, String password){
+        Connexion connexion = new Connexion();
+        connexion.connexionToUser(mail, password);
     }
 
     public void boutonAppuye(){
