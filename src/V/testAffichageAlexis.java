5dd0ed7f230e88370c74de2792b74a5135b481f6
Ashtaggg <alexis1.raynal@gmail.com>
2024-04-07 17:12:11 +0200

    ajout dans bdd

--- src/V/testAffichageAlexis.java
+++ src/V/testAffichageAlexis.java
@@ -8,6 +8,7 @@ import java.text.ParseException;
 
 import C.MainAlexis;
 import M.JAVA_MODEL.Global_CLASS.Film;
+import M.DAO.DAO_MYSQL_WAMP.Films.FilmsDAO_IMPL;
 
 public class testAffichageAlexis {
     JFrame test;
@@ -19,13 +20,14 @@ public class testAffichageAlexis {
     public void affichage(){
         MainAlexis main = new MainAlexis();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        FilmsDAO_IMPL filmsDAO = new FilmsDAO_IMPL();
         Date dateSortie = null;
         try {
             dateSortie = sdf.parse("2002-02-02");
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        Film film = new Film(112, "test", 2, dateSortie, "", "1:00:00", "edhgdh", 4, 0, "jvjb", "khgu", "uygsvi");
+        Film film = new Film(112, "test", 2, dateSortie, "", "1:00:00", "edhgdh", 4.2, 0, "jvjb", "khgu", "uygsvi");
         
         test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         test.setLayout(new GridLayout(20, 10));
@@ -37,6 +39,7 @@ public class testAffichageAlexis {
         bouton.addActionListener(e -> {
             System.out.println("Appuyé");
             main.boutonAppuye();
+            filmsDAO.ajouterFilm(film);
         });
         bouton.setPreferredSize(new Dimension(50, 50));
         test.add(bouton);
