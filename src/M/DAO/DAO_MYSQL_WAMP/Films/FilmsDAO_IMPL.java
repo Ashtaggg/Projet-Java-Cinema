7b3dadc6f7529af96498782587a08a0f919046c5
Ashtaggg <alexis1.raynal@gmail.com>
2024-04-10 01:54:50 +0200

    plein de trucs

--- src/M/DAO/DAO_MYSQL_WAMP/Films/FilmsDAO_IMPL.java
+++ src/M/DAO/DAO_MYSQL_WAMP/Films/FilmsDAO_IMPL.java
@@ -37,7 +37,31 @@ public class FilmsDAO_IMPL implements FilmsDAO{
     }
 
     public void modifierFilm(Film film) {
-        // A faire
+        Connection connexion = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connexion = DAOFactory.getConnection();
+            preparedStatement = connexion.prepareStatement("UPDATE film SET Nom = ?, Prix = ?, DateSortie = ?, Photo = ?, Duree = ?, Description = ?, Note = ?, 4DX = ?, Realisateur = ?, Acteur = ?, Synopsis = ? WHERE ID_Film = ?;");
+            preparedStatement.setString(1, film.getNom());
+            preparedStatement.setDouble(2, film.getPrix());
+            java.sql.Date dateSql = new java.sql.Date(film.getDateSortie().getTime());
+            preparedStatement.setDate(3, dateSql);
+            preparedStatement.setString(4, film.getPhoto());
+            preparedStatement.setString(5, film.getDuree());
+            preparedStatement.setString(6, film.getDescription());
+            preparedStatement.setDouble(7, film.getNote());
+            preparedStatement.setInt(8, film.getQuatreDX());
+            preparedStatement.setString(9, film.getRealisateur());
+            preparedStatement.setString(10, film.getActeur());
+            preparedStatement.setString(11, film.getSynopsis());
+            preparedStatement.setInt(12, film.getIdFilm());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void modifierFilmByID(int ID) {
@@ -58,7 +82,39 @@ public class FilmsDAO_IMPL implements FilmsDAO{
     }
 
     public Film recupererFilmByTitre(String titre) {
-        // A faire
+        Connection connexion = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connexion = DAOFactory.getConnection();
+            preparedStatement = connexion.prepareStatement("SELECT * FROM film WHERE Nom = ?");
+            preparedStatement.setString(1, titre);
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            if(result.next()) {
+                int idFilm = result.getInt("ID_Film");
+                String nom = result.getString("Nom");
+                float prix = result.getFloat("Prix");
+                Date dateSortie = result.getDate("DateSortie");
+                String photo = result.getString("Photo");
+                String duree = result.getString("Duree");
+                String description = result.getString("Description");
+                double note = result.getDouble("Note");
+                int quatreDX = result.getInt("4DX");
+                String realisateur = result.getString("Realisateur");
+                String acteur = result.getString("Acteur");
+                String synopsis = result.getString("Synopsis");
+
+                Film film = new Film(idFilm, nom, prix, dateSortie, photo, duree, description, note, quatreDX, realisateur, acteur, synopsis);
+
+                return film;
+            }
+
+        } catch (SQLException error) {
+            System.out.println(error);
+        }
+
         return null;
     }
 
---
+++ src/M/JAVA_MODEL/ConversionImageBDD/ConvertirImageHexa.java
@@ -0,0 +1,67 @@
+package M.JAVA_MODEL.ConversionImageBDD;
+
+import javax.swing.*;
+import java.io.*;
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+import V.FrameBase;
+
+
+
+public class ConvertirImageHexa {
+    public static String ImageToHex(String path){ // --------------------------- Convertir Image (depuis un path) en Hexa ---------------------------
+        String imageHex = "";
+    
+        try {
+            File file = new File(path);
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            StringBuilder hexString = new StringBuilder();
+
+            while (bis.available() > 0) {
+                int value = bis.read();
+                hexString.append(String.format("%02X", value));
+            }
+
+            bis.close();
+            imageHex = hexString.toString();
+            return imageHex;
+        } catch (IOException error) {
+            System.out.println(error);
+        }
+        return imageHex;
+    }
+
+
+
+
+    public static void HexTiImage(FrameBase frame, String imageHex, int x, int y, int width, int height){// --------------- Convertir Hexa en Image et l'afficher ------------------
+        if (!imageHex.isEmpty()) {
+            byte[] bytes = new byte[imageHex.length() / 2];
+            for (int i = 0; i < imageHex.length(); i += 2) {
+                bytes[i / 2] = (byte) Integer.parseInt(imageHex.substring(i, i + 2), 16);
+            }
+            ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+
+
+            BufferedImage image = null;
+            try {
+                image = ImageIO.read(bis);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            JLabel label = new JLabel(new ImageIcon(image));
+
+            label.setBounds(x, y, width, height);
+
+            frame.getPanelBase().setVisible(true);
+            frame.getPanelBase().add(label);
+            frame.RefreshPage();
+        }
+    }
+}
